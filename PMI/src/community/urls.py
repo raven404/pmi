3ba63a4a8d0091a3613ad8d9c50ac97b4347721d
Mritunjay Srivastava <mritunjaysri01@gmail.com>
2026-08-29 14:52:34 +0530

    Delete PMI directory

--- PMI/src/community/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-app_name = 'community'
-
-urlpatterns = [
-
-    path('Nandlal-community/', views.Nandlal, name='NandlalCommunity'),
-    path('Badabagh-community/', views.Badabagh, name='BadabaghCommunity'),
-]
